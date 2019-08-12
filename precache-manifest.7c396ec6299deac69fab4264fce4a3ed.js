c9057e477797c7d685ae22ab91fc5e2de06a5fcd
july26th <hien.tran.nmmt@gmail.com>
2019-08-12 14:52:46 +0700

    Updates

---
+++ precache-manifest.7c396ec6299deac69fab4264fce4a3ed.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d9c11f429bf71bb482f03b5c00e125e7",
+    "url": "/shopping-cart/index.html"
+  },
+  {
+    "revision": "f5eef3ca988f4440ca5e",
+    "url": "/shopping-cart/static/css/main.1af5addc.chunk.css"
+  },
+  {
+    "revision": "dcb35ea75eeab51b7947",
+    "url": "/shopping-cart/static/js/2.4c71a5e2.chunk.js"
+  },
+  {
+    "revision": "f5eef3ca988f4440ca5e",
+    "url": "/shopping-cart/static/js/main.212a1080.chunk.js"
+  },
+  {
+    "revision": "48120009b25447af4028",
+    "url": "/shopping-cart/static/js/runtime~main.fca8da01.js"
+  },
+  {
+    "revision": "7690bd224c84c3d6dd9b8aa55384fc8b",
+    "url": "/shopping-cart/static/media/logo.7690bd22.svg"
+  }
+]);
